ea7acc2588a35a6648d6a7029ca24cfcc4cc706a
leejunseok <wnstjr120422@naver.com>
2024-01-12 14:14:39 +0900

    feat : 들여쓰기 정리

--- src/main/java/utils/view/InputView.java
+++ src/main/java/utils/view/InputView.java
@@ -3,11 +3,11 @@ package utils.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static void inputMessage(String message){
+    public static void inputMessage(String message) {
         System.out.println(message);
     }
 
-    public static String input(){
+    public static String input() {
         return Console.readLine();
     }
 }
--- src/main/java/utils/view/OutputView.java
+++ src/main/java/utils/view/OutputView.java
@@ -1,15 +1,11 @@
 package utils.view;
 
 public class OutputView {
-    public final static String STRIKE="스트라이크";
-    public final static String BALL="볼";
-    public final static String NOTHING="낫싱";
-
-    public static void outputView(String message){
+    public static void outputView(String message) {
         System.out.print(message);
     }
 
-    public static void resultView(String message){
-       System.out.println(message);
+    public static void resultView(String message) {
+        System.out.println(message);
     }
 }
