4bb81808451c6b6cd0494934a363791d8876e01a
leejunseok <wnstjr120422@naver.com>
2024-01-12 14:14:15 +0900

    feat : 정적 메시지 선언 클래스 추가

---
+++ src/main/java/config/exception/ErrorStatus.java
@@ -0,0 +1,6 @@
+package config.exception;
+
+public class ErrorStatus {
+    public final static String NOT_EXIST_TYPE = "존재하지 않는 타입입니다";
+    public final static String IS_NOT_NUMBER = "문자를 입력하였습니다.";
+}
---
+++ src/main/java/config/status/Message.java
@@ -0,0 +1,14 @@
+package config.status;
+
+public class Message {
+    public final static String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public final static String GAMEOVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public final static String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String INPUT_MESSAGE = "숫자를 입력해주세요 :";
+    public final static String STRIKE = "스트라이크";
+    public final static String BALL = "볼";
+    public final static String NOTHING = "낫싱";
+    public final static String EMPTY_ERROR_MESSAGE = "문자를 입력하지 않았습니다";
+    public final static String LENGTH_ERROR_MESSAGE = "입력한 문자가 3개가 아닙니다";
+    public final static String DUPLICATE_ERROR_MESSAGE = "중복된 문자를 입력하였습니다";
+}
