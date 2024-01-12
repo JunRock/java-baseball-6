a798de8174d89b3a73a1e0bad58b7f9213839534
leejunseok <wnstjr120422@naver.com>
2024-01-12 14:15:13 +0900

    feat : 입력한 문자가 숫자인지 판별 추가

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -6,15 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static config.exception.ErrorStatus.IS_NOT_NUMBER;
+import static config.status.Condition.NUMBER_LENGTH;
+import static config.status.Message.*;
+
 public class Player {
-
-    private final static String EMPTY_ERROR_MESSAGE="문자를 입력하지 않았습니다";
-    private final static String LENGTH_ERROR_MESSAGE="입력한 문자가 3개가 아닙니다";
-    private final static String DUPLICATE_ERROR_MESSAGE="중복된 문자를 입력하였습니다";
-    private final static int NUMBER_LENGTH=3;
-
-    public List<Integer> inputNumber(){
+    public List<Integer> inputNumber() {
         String number = InputView.input();
+        validIsNumber(number);
         validIsEmpty(number);
         validLengthOfNumber(number);
         validDuplicateNumber(number);
@@ -24,43 +23,63 @@ public class Player {
     /**
      * 아무것도 입력하지 않은 경우
      */
-    public void validIsEmpty(String number){
-        if(number.isEmpty())
+    public void validIsEmpty(String number) {
+        if (number.isEmpty())
             throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
     }
 
     /**
      * 입력한 길이가 부족한 경우
      */
-    public void validLengthOfNumber(String number){
-        if(number.length()!=NUMBER_LENGTH)
+    public void validLengthOfNumber(String number) {
+        if (number.length() != NUMBER_LENGTH)
             throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
     }
 
     /**
      * 중복된 숫자를 입력한 경우
      */
-
-    public void validDuplicateNumber(String number){
-        if(isUserNumber(number))
+    public void validDuplicateNumber(String number) {
+        if (isUserNumber(number))
             throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
     }
 
+    /**
+     * 숫자를 입력하지 않은 경우
+     */
+
+    public void validIsNumber(String number) {
+        if (!isNumber(number))
+            throw new IllegalArgumentException(IS_NOT_NUMBER);
+    }
+
+    private boolean isNumber(String number) {
+        return number
+                .chars()
+                .anyMatch(Character::isDigit);
+    }
+
     private boolean isUserNumber(String number) {
-        boolean userNumber= Arrays.asList(number.split(""))
+        boolean userNumber = getList(number)
                 .stream()
                 .distinct()
-                .count()!= number.length();
+                .count() != number.length();
         return userNumber;
     }
 
-    private List<Integer> converterInteger(String number){
-        return Arrays.asList(number.split(""))
+    private List<Integer> converterInteger(String number) {
+        return getList(number)
                 .stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
-    public static Integer restartOption(){
+
+    public static Integer restartOption() {
         return Integer.valueOf(InputView.input());
     }
+
+    private List<String> getList(String number) {
+        return Arrays.asList(number.split(""));
+    }
+
 }
